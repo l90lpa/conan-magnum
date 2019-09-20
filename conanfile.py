b57f368b5cf22b52433874b053f0daa6d655603b
Devin Schwab <digidevin@gmail.com>
2019-09-20 15:38:03 -0400

    :bug: Not including static plugins in libs list

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
 import os
+from pathlib import Path
 
-def sort_libs(correct_order, libs, lib_suffix='', reverse_result=False):
+from conans import CMake, ConanFile, tools
+
+
+def sort_libs(correct_order, libs, lib_suffix="", reverse_result=False):
     # Add suffix for correct string matching
     correct_order[:] = [s.__add__(lib_suffix) for s in correct_order]
 
@@ -20,26 +23,31 @@ def sort_libs(correct_order, libs, lib_suffix='', reverse_result=False):
 
     return result
 
+
 class LibnameConan(ConanFile):
     name = "magnum"
     version = "2019.01"
-    description =   "Magnum — Lightweight and modular C++11/C++14 \
+    description = "Magnum — Lightweight and modular C++11/C++14 \
                     graphics middleware for games and data visualization"
     # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
     topics = ("conan", "corrade", "graphics", "rendering", "3d", "2d", "opengl")
     url = "https://github.com/helmesjo/conan-magnum"
     homepage = "https://magnum.graphics"
     author = "helmesjo <helmesjo@gmail.com>"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    license = (
+        "MIT"
+    )  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-    short_paths = True  # Some folders go out of the 260 chars path length scope (windows)
+    short_paths = (
+        True
+    )  # Some folders go out of the 260 chars path length scope (windows)
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "shared": [True, False], 
+        "shared": [True, False],
         "fPIC": [True, False],
         "build_deprecated": [True, False],
         "build_multithreaded": [True, False],
@@ -79,7 +87,7 @@ class LibnameConan(ConanFile):
         "with_xeglapplication": [True, False],
     }
     default_options = {
-        "shared": False, 
+        "shared": False,
         "fPIC": True,
         "build_deprecated": True,
         "build_multithreaded": True,
@@ -123,28 +131,26 @@ class LibnameConan(ConanFile):
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
-    requires = (
-        "corrade/2019.01@rhololkeolke/stable"
-    )
+    requires = "corrade/2019.01@rhololkeolke/stable"
 
     def system_package_architecture(self):
         if tools.os_info.with_apt:
             if self.settings.arch == "x86":
-                return ':i386'
+                return ":i386"
             elif self.settings.arch == "x86_64":
-                return ':amd64'
+                return ":amd64"
             elif self.settings.arch == "armv6" or self.settings.arch == "armv7":
-                return ':armel'
+                return ":armel"
             elif self.settings.arch == "armv7hf":
-                return ':armhf'
+                return ":armhf"
             elif self.settings.arch == "armv8":
-                return ':arm64'
+                return ":arm64"
 
         if tools.os_info.with_yum:
             if self.settings.arch == "x86":
-                return '.i686'
-            elif self.settings.arch == 'x86_64':
-                return '.x86_64'
+                return ".i686"
+            elif self.settings.arch == "x86_64":
+                return ".x86_64"
         return ""
 
     def system_requirements(self):
@@ -176,19 +182,23 @@ class LibnameConan(ConanFile):
                 for package in packages:
                     installer.install("%s%s" % (package, arch_suffix))
             else:
-                self.output.warn("Could not determine package manager, skipping Linux system requirements installation.")
+                self.output.warn(
+                    "Could not determine package manager, skipping Linux system requirements installation."
+                )
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        self.options['corrade'].add_option('build_deprecated', self.options.build_deprecated)
+        self.options["corrade"].add_option(
+            "build_deprecated", self.options.build_deprecated
+        )
 
         # To fix issue with resource management, see here:
         # https://github.com/mosra/magnum/issues/304#issuecomment-451768389
         if self.options.shared:
-            self.options['corrade'].add_option('shared', True)
+            self.options["corrade"].add_option("shared", True)
 
     def requirements(self):
         if self.options.with_sdl2application:
@@ -210,7 +220,13 @@ class LibnameConan(ConanFile):
         def add_cmake_option(option, value):
             var_name = "{}".format(option).upper()
             value_str = "{}".format(value)
-            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str 
+            var_value = (
+                "ON"
+                if value_str == "True"
+                else "OFF"
+                if value_str == "False"
+                else value_str
+            )
             cmake.definitions[var_name] = var_value
 
         for option, value in self.options.items():
@@ -221,7 +237,10 @@ class LibnameConan(ConanFile):
         add_cmake_option("LIB_SUFFIX", "")
 
         add_cmake_option("BUILD_STATIC", not self.options.shared)
-        add_cmake_option("BUILD_STATIC_PIC", not self.options.shared and self.options.get_safe("fPIC"))
+        add_cmake_option(
+            "BUILD_STATIC_PIC",
+            not self.options.shared and self.options.get_safe("fPIC"),
+        )
 
         cmake.configure(build_folder=self._build_subfolder)
 
@@ -239,17 +258,17 @@ class LibnameConan(ConanFile):
     def package_info(self):
         # See dependency order here: https://doc.magnum.graphics/magnum/custom-buildsystems.html
         allLibs = [
-            #1
+            # 1
             "Magnum",
             "MagnumAnimation",
             "MagnumMath",
-            #2
+            # 2
             "MagnumAudio",
             "MagnumGL",
             "MagnumSceneGraph",
             "MagnumTrade",
             "MagnumVk",
-            #3
+            # 3
             "MagnumMeshTools",
             "MagnumPrimitives",
             "MagnumShaders",
@@ -257,20 +276,25 @@ class LibnameConan(ConanFile):
             "MagnumGlfwApplication",
             "MagnumXEglApplication",
             "MagnumWindowlessEglApplication",
-            "MagnumGlxApplication" ,
+            "MagnumGlxApplication",
             "MagnumWindowlessGlxApplication",
             "MagnumSdl2Application",
             "MagnumWindowlessSdl2Application",
-            #4
+            # 4
             "MagnumDebugTools",
             "MagnumOpenGLTester",
             "MagnumText",
         ]
-        
+
         # Sort all built libs according to above, and reverse result for correct link order
-        suffix = '-d' if self.settings.build_type == "Debug" else ''
+        suffix = "-d" if self.settings.build_type == "Debug" else ""
         builtLibs = tools.collect_libs(self)
-        self.cpp_info.libs = sort_libs(correct_order=allLibs, libs=builtLibs, lib_suffix=suffix, reverse_result=True)
+        self.cpp_info.libs = sort_libs(
+            correct_order=allLibs,
+            libs=builtLibs,
+            lib_suffix=suffix,
+            reverse_result=True,
+        )
 
         if self.settings.os == "Windows":
             if self.settings.compiler == "Visual Studio":
@@ -283,3 +307,21 @@ class LibnameConan(ConanFile):
                 self.cpp_info.exelinkflags.append("-framework OpenGL")
             elif not self.options.shared:
                 self.cpp_info.libs.append("GL")
+
+        # find static plugin libraries and add them to the linker list
+        if self.options.build_plugins_static:
+            importer_libs_path = (
+                Path("lib")
+                / f"magnum{'-d' if self.settings.build_type == 'Debug' else ''}"
+                / "importers"
+            )
+
+            self.cpp_info.libdirs.append(str(importer_libs_path))
+
+            # make relative to conan rootdir
+            importer_libs_path = Path(self.cpp_info.rootpath) / importer_libs_path
+
+            importer_libs = []
+            for importer_lib in importer_libs_path.glob("*.a"):
+                importer_libs.append(importer_lib.stem[3:])
+            self.cpp_info.libs = importer_libs + self.cpp_info.libs                
